d2d5539435d9e9f20a8bfd68f934816a4c6c64e0
Andres Mastracchio <mastracchioandres@gmail.com>
2021-01-25 12:35:15 -0300

    Added when firing validation

--- app/src/main/java/com/example/mod2unidad2ejercito/MainActivity.kt
+++ app/src/main/java/com/example/mod2unidad2ejercito/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.mod2unidad2ejercito
 
+import android.annotation.SuppressLint
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -9,48 +11,99 @@ import android.widget.EditText
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
-import com.example.mod2unidad2ejercito.entities.Barco
-import com.example.mod2unidad2ejercito.entities.Portaavion
-import com.example.mod2unidad2ejercito.entities.Submarino
+import com.example.mod2unidad2ejercito.entities.*
 
 class MainActivity : AppCompatActivity() {
 
+    // => variables inicializadas en métodos mover() & abrirFuego()
     lateinit var textViewMover: TextView
     lateinit var textViewAbrirFuego: TextView
     lateinit var textViewLat1: EditText
     lateinit var textViewLong1: EditText
     lateinit var textViewLat2: EditText
     lateinit var textViewLong2: EditText
-    //lateinit var radioButtonSelected: RadioButton
+    lateinit var radioButtonAvion: RadioButton
+    lateinit var radioButtonTanque: RadioButton
+    lateinit var radioButtonSubmarino: RadioButton
+
+    // => instancias utilizadas en el onCreate()
+    val ejercito = Ejercito()
+    val avion = Avion()
+    val barco = Barco()
+    val jeep = Jeep()
+    val portaavion = Portaavion()
+    val submarino = Submarino()
+    val tanque = Tanque()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        ejercito.flota.add(barco.id)
+        ejercito.flota.add(portaavion.id)
+        ejercito.flota.add(submarino.id)
+        ejercito.divisionTerrestre.add(jeep)
+        ejercito.divisionTerrestre.add(tanque)
+        ejercito.divisionAerea.add(avion)
+
+        mostrarFlota()
+        mostrarDivTerrestre()
+        mostrarDivAerea()
+
+        // => views que muestran resultado en pantalla
         textViewMover = findViewById(R.id.textViewResultMover)
         textViewAbrirFuego = findViewById(R.id.textViewResultAbrirFuego)
     }
 
+    /*
+    Métodos que muestran listados por consola
+     */
+    fun mostrarFlota(){
+        for (veh in ejercito.flota){
+            println(veh)
+        }
+    }
+    fun mostrarDivTerrestre(){
+        for (veh in ejercito.divisionTerrestre){
+            println(veh)
+        }
+    }
+    fun mostrarDivAerea(){
+        for (veh in ejercito.divisionAerea){
+            println(veh)
+        }
+    }
+
+    /*
+    Toma valores del EditText y los muestra por medio de textViewMover que se devuelve a la vista en el onCreate()
+    */
+    @SuppressLint("SetTextI18n") //Elimina resaltado de Lint
     fun mover(view: View) : TextView{
         textViewLat1 = findViewById(R.id.editTextMoverLatitud)
         textViewLong1 = findViewById(R.id.editTextMoverLongitud)
 
-        textViewMover.text = "Moviendo hacia las coordenadas: \n +" +
+        textViewMover.text = "Moviendo hacia las coordenadas: \n" +
                 "${textViewLat1.text} | ${textViewLong1.text}"
         return textViewMover
     }
 
+    /*
+    Toma valores del EditText y los muestra por medio de textViewMover que se devuelve a la vista en el onCreate()
+    */
+    @SuppressLint("SetTextI18n") //Elimina resaltado de Lint
     fun abrirFuego(view: View): TextView{
         textViewLat2 = findViewById(R.id.editTextAbrirFuegoLatitud)
         textViewLong2 = findViewById(R.id.editTextAbrirFuegoLongitud)
+        radioButtonAvion = findViewById(R.id.radioButtonAvion)
+        radioButtonTanque = findViewById(R.id.radioButtonTanque)
+        radioButtonSubmarino = findViewById(R.id.radioButtonSubmarino)
 
-        /*if (radioButtonSelected.text == "AVIÓN" || radioButtonSelected.text == "TANQUE" || radioButtonSelected.text == "SUBMARINO") {
+        if (radioButtonAvion.isChecked || radioButtonTanque.isChecked || radioButtonSubmarino.isChecked) {
             textViewAbrirFuego.text = "¡Abriendo fuego en las coordenadas!: \n" +
-                    " ${textViewLat2.text} | ${textViewLong2.text}"
+                " ${textViewLat2.text} | ${textViewLong2.text}"
         } else {
             textViewAbrirFuego.text = "No se puede abrir fuego con el vehículo seleccionado"
-        }*/
-
+        }
         return textViewAbrirFuego
     }
 
--- app/src/main/java/com/example/mod2unidad2ejercito/entities/Ejercito.kt
+++ app/src/main/java/com/example/mod2unidad2ejercito/entities/Ejercito.kt
@@ -2,5 +2,9 @@ package com.example.mod2unidad2ejercito.entities
 
 class Ejercito {
 
+    val flota = mutableListOf<Any>() //Barco, Portaavion, Submarino
+    val divisionTerrestre = mutableListOf<Any>() //Jeep, Tanque
+    val divisionAerea = mutableListOf<Any>() //Avion
+
 
 }
